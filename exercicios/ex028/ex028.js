0405b30f3abc9a6d4bfea48f51a0480bf4f9342b
carlosmortari <113060314+carlosmortari@users.noreply.github.com>
2023-05-12 13:47:30 -0300

    ok

---
+++ exercicios/ex028/ex028.js
@@ -0,0 +1,8 @@
+var velo = 66
+
+console.log(`A velocidade do seu carro é ${velo} Km/h`)
+if (velo > 65) {
+    console.log(`Voce ultrapassou a velocidade permitida [MULTADO]`)
+}else {
+    console.log('Não se esqueça de usar sempre o cinto de segurança')
+}
