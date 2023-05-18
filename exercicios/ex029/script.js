3aa8ddfe38d96d474d3b2cdce783be718e219434
carlosmortari <113060314+carlosmortari@users.noreply.github.com>
2023-05-18 08:13:06 -0300

    novo

--- exercicios/ex029/script.js
+++ exercicios/ex029/script.js
@@ -1,7 +1,21 @@
 var num = [5, 2, 8, 6, 3, 7]
 num.sort()
-
+/*
 console.log(`Esses são os valores dentro do Vetor:`)
     for(c = 0;c <= num.length; c = c+1) {
         console.log(`A posição  ${c} do vetor tem o valor ${num[c]} `)
+   }
+   */
+
+  /*  for(c = 0; c <= num.length; c = c+1) {
+    console.log(`A posição ${c} do vetor tem o valor ${num[c]}`)
+   }
+   */
+
+   for(c = 0; c < num.length; c++) {
+    if( num[c]%2 == 1){
+    console.log(`O valor ${num[c]} É impar`)
+    }else {
+        console.log(`O valor ${num[c]} É par`)
+    }
    }
