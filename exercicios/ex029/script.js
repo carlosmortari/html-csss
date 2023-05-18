7d2ce49c9f16a2abd7bba6ee2c10b084f76438eb
carlosmortari <113060314+carlosmortari@users.noreply.github.com>
2023-05-18 07:45:37 -0300

    Update script.js

--- exercicios/ex029/script.js
+++ exercicios/ex029/script.js
@@ -4,4 +4,4 @@ num.sort()
 console.log(`Esses são os valores dentro do Vetor:`)
     for(c = 0;c <= num.length; c = c+1) {
         console.log(`A posição  ${c} do vetor tem o valor ${num[c]} `)
-    }
+   }     
